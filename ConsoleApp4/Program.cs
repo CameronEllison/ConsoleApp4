47bbde5b198ccfbf52bb39460e2011b33c892b7c
Cameron K Ellison <cameronellison@hotmail.com>
2019-05-03 18:37:12 -0500

    First Commit

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!")
+                 Console.WriteLine("Hello World!");
         }
     }
 }
